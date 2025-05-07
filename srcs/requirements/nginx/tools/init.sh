25590b0fb9a49612850a556f9b647c80542792e2
Mees Dekker <me@meesdekker.xyz>
2025-05-07 15:12:42 +0200

    changes

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -12,7 +12,7 @@ if [ ! -f /etc/nginx/ssl/nginx.crt ] || [ ! -f /etc/nginx/ssl/nginx.key ]; then
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/nginx/ssl/nginx.key \
     -out /etc/nginx/ssl/nginx.crt \
-    -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=student/CN=${DOMAIN_NAME}"
+    -subj "/C=NL/ST=Amsterdam/L=Amsterdam/O=Codam/OU=mdekker/CN=${DOMAIN_NAME}"
   echo "SSL certificate generated."
 else
   echo "SSL certificate already exists."
